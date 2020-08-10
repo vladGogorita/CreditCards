afcda9a47000746b55fbf8dbf9143004c99cdcd0
vladGogorita <vlgogorita@gmail.com>
2020-08-10 23:58:15 +0300

    Initial commit

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/");
+
+                DemoHelper.Pause();
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Home Page - Credit Cards", pageTitle);
+                Assert.Equal("http://localhost:44108/", driver.Url);
+            }
+        }
+    }
+}
---
+++ CreditCards.UITests/DemoHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace CreditCards.UITests
+{
+    internal static class DemoHelper
+    {
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+    }
+}
